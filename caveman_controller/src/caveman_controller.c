1825226f926fbe550403645cb5ee3fc371d629ba
delta-12 <btesq235@gmail.com>
2025-02-17 17:04:47 -0500

    SD-151 motor servo test

--- caveman_controller/src/caveman_controller.c
+++ caveman_controller/src/caveman_controller.c
@@ -9,6 +9,7 @@
 #include "bsp_encoder.h"
 #include "bsp_encoder_user.h"
 #include "bsp_logger.h"
+#include "bsp_pwm.h"
 #include "bsp_pwm_user.h"
 #include "bsp_servo.h"
 #include "bsp_tick.h"
@@ -30,67 +31,89 @@ int main(void)
     BspServo_Handle_t wheel_0_servo = {
         .timer              = BSP_PWM_USER_TIMER_STEERING_SERVOS,
         .channel            = BSP_TIMER_CHANNEL_1,
-        .minimum_duty_cycle = 0,
+        .minimum_duty_cycle = 0.025,
         .maximum_duty_cycle = 0.125,
         .minimum_angle      = 0,
-        .maximum_angle      = 120,
+        .maximum_angle      = 180,
     };
     BspServo_Handle_t wheel_1_servo = {
         .timer              = BSP_PWM_USER_TIMER_STEERING_SERVOS,
         .channel            = BSP_TIMER_CHANNEL_2,
-        .minimum_duty_cycle = 0,
+        .minimum_duty_cycle = 0.025,
         .maximum_duty_cycle = 0.125,
         .minimum_angle      = 0,
-        .maximum_angle      = 120,
+        .maximum_angle      = 180,
     };
     BspServo_Handle_t wheel_2_servo = {
         .timer              = BSP_PWM_USER_TIMER_STEERING_SERVOS,
         .channel            = BSP_TIMER_CHANNEL_3,
-        .minimum_duty_cycle = 0,
+        .minimum_duty_cycle = 0.025,
         .maximum_duty_cycle = 0.125,
         .minimum_angle      = 0,
-        .maximum_angle      = 120,
+        .maximum_angle      = 180,
     };
     BspServo_Handle_t wheel_3_servo = {
         .timer              = BSP_PWM_USER_TIMER_STEERING_SERVOS,
         .channel            = BSP_TIMER_CHANNEL_4,
-        .minimum_duty_cycle = 0,
+        .minimum_duty_cycle = 0.025,
         .maximum_duty_cycle = 0.125,
         .minimum_angle      = 0,
-        .maximum_angle      = 120,
+        .maximum_angle      = 180,
     };
 
+    HAL_Delay(2000);
+
+    BspPwm_Start(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_1);
+    BspPwm_Start(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_2);
+    BspPwm_Start(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_3);
+    BspPwm_Start(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_4);
     BspServo_Start(&wheel_0_servo);
     BspServo_Start(&wheel_1_servo);
     BspServo_Start(&wheel_2_servo);
     BspServo_Start(&wheel_3_servo);
 
-    // BspServo_SetAngle(&wheel_0_servo, 0);
-    // BspServo_SetAngle(&wheel_1_servo, 0);
-    // BspServo_SetAngle(&wheel_2_servo, 0);
-    // BspServo_SetAngle(&wheel_3_servo, 0);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_1, 0.10);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_2, 0.10);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_3, 0.10);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_4, 0.10);
+    BspServo_SetAngle(&wheel_0_servo, 0);
+    BspServo_SetAngle(&wheel_1_servo, 0);
+    BspServo_SetAngle(&wheel_2_servo, 0);
+    BspServo_SetAngle(&wheel_3_servo, 0);
 
+    HAL_Delay(3000);
+
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_1, 0.50);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_2, 0.50);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_3, 0.50);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_4, 0.50);
     BspServo_SetAngle(&wheel_0_servo, 60);
-    // HAL_Delay(3000);
     BspServo_SetAngle(&wheel_1_servo, 60);
-    // HAL_Delay(3000);
     BspServo_SetAngle(&wheel_2_servo, 60);
-    // HAL_Delay(3000);
     BspServo_SetAngle(&wheel_3_servo, 60);
 
-    // HAL_Delay(1000);
+    HAL_Delay(3000);
 
-    // BspServo_SetAngle(&wheel_0_servo, 120);
-    // BspServo_SetAngle(&wheel_1_servo, 120);
-    // BspServo_SetAngle(&wheel_2_servo, 120);
-    // BspServo_SetAngle(&wheel_3_servo, 120);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_1, 1.0);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_2, 1.0);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_3, 1.0);
+    BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_4, 1.0);
+    BspServo_SetAngle(&wheel_0_servo, 120);
+    BspServo_SetAngle(&wheel_1_servo, 120);
+    BspServo_SetAngle(&wheel_2_servo, 120);
+    BspServo_SetAngle(&wheel_3_servo, 120);
 
-    // BspPwm_Start(BSP_PWM_USER_TIMER_STEERING_SERVOS, BSP_TIMER_CHANNEL_1);
-    // BspPwm_SetDutyCycle(BSP_PWM_USER_TIMER_STEERING_SERVOS, BSP_TIMER_CHANNEL_1, 0.075);
+    HAL_Delay(3000);
 
-    BSP_LOGGER_LOG_DEBUG(kCaveman_LogTag, "%u", (uint32_t)BspTick_GetMicroseconds());
+    BspPwm_Stop(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_1);
+    BspPwm_Stop(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_2);
+    BspPwm_Stop(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_3);
+    BspPwm_Stop(BSP_PWM_USER_TIMER_MOTORS, BSP_TIMER_CHANNEL_4);
 
-    BSP_LOGGER_LOG_DEBUG(kCaveman_LogTag, "Done");
+    BspServo_Stop(&wheel_0_servo);
+    BspServo_Stop(&wheel_1_servo);
+    BspServo_Stop(&wheel_2_servo);
+    BspServo_Stop(&wheel_3_servo);
 
     BspEncoderUser_Pulse_t pulses = BspEncoderUser_HandleTable[BSP_ENCODER_USER_TIMER_0].pulses;
     while (true)

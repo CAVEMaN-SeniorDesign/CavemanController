c73030ce2d0a8e26a1bae36383e084cf1999ffd7
delta-12 <btesq235@gmail.com>
2025-02-26 01:14:50 -0500

    SD-124 cave talk implementation

--- caveman_controller/src/caveman_cavetalk.c
+++ caveman_controller/src/caveman_cavetalk.c
@@ -13,12 +13,20 @@
 #define CAVEMAN_CAVE_TALK_BUFFER_SIZE 1024U
 #define CAVEMAN_CAVE_TALK_HEADER_SIZE 3U
 
-static uint8_t CavemanCaveTalk_Buffer[CAVEMAN_CAVE_TALK_BUFFER_SIZE];
+typedef enum
+{
+    CAVEMAN_CAVE_TALK_RECEIVE_HEADER,
+    CAVEMAN_CAVE_TALK_RECEIVE_PAYLOAD
+} CavemanCaveTalk_Receive_t;
+
+static uint8_t                   CavemanCaveTalk_Buffer[CAVEMAN_CAVE_TALK_BUFFER_SIZE];
+static CavemanCaveTalk_Receive_t CavemanCaveTalk_LastReceive = CAVEMAN_CAVE_TALK_RECEIVE_HEADER;
 
 static void CavemanCaveTalk_RxCallback(Bsp_Uart_t *const uart);
 static CaveTalk_Error_t CavemanCaveTalk_Send(const void *const data, const size_t size);
 static CaveTalk_Error_t CavemanCaveTalk_Receive(void *const data, const size_t size, size_t *const bytes_received);
 static CaveTalk_Error_t CavemanCaveTalk_Available(size_t *const bytes_available);
+static CaveTalk_Error_t CavemanCaveTalk_ConvertBspError(const Bsp_Error_t bsp_error);
 
 CaveTalk_Handle_t CavemanCaveTalk_Handle = {
     .link_handle = {
@@ -33,15 +41,59 @@ CaveTalk_Handle_t CavemanCaveTalk_Handle = {
 
 Bsp_Error_t CavemanCaveTalk_Start(void)
 {
-    return BspUart_Start(BSP_UART_USER_COMMS, NULL, CavemanCaveTalk_RxCallback);
+    Bsp_Error_t error = BspUart_Start(BSP_UART_USER_COMMS, NULL, CavemanCaveTalk_RxCallback);
+
+    if (BSP_ERROR_NONE == error)
+    {
+        CavemanCaveTalk_LastReceive = CAVEMAN_CAVE_TALK_RECEIVE_HEADER;
+        error                       = BspUart_StartReceive(BSP_UART_USER_COMMS, CAVEMAN_CAVE_TALK_HEADER_SIZE);
+    }
+
+    return error;
 }
 
 static CaveTalk_Error_t CavemanCaveTalk_Send(const void *const data, const size_t size)
 {
-    CaveTalk_Error_t cavetalk_error = CAVE_TALK_ERROR_NONE;
-    Bsp_Error_t      bsp_error      = BspUart_Transmit(BSP_UART_USER_COMMS, data, size);
+    return CavemanCaveTalk_ConvertBspError(BspUart_StartTransmit(BSP_UART_USER_COMMS, data, size));
+}
 
-    /* BspUart_Transmit should not return BSP_ERROR_VALUE */
+static CaveTalk_Error_t CavemanCaveTalk_Receive(void *const data, const size_t size, size_t *const bytes_received)
+{
+    return CavemanCaveTalk_ConvertBspError(BspUart_Read(BSP_UART_USER_COMMS, data, size, bytes_received));
+}
+
+static CaveTalk_Error_t CavemanCaveTalk_Available(size_t *const bytes_available)
+{
+    return CavemanCaveTalk_ConvertBspError(BspUart_Available(BSP_UART_USER_COMMS, bytes_available));
+}
+
+static void CavemanCaveTalk_RxCallback(Bsp_Uart_t *const uart)
+{
+    uint8_t length = CAVEMAN_CAVE_TALK_HEADER_SIZE;
+
+    switch (CavemanCaveTalk_LastReceive)
+    {
+    case CAVEMAN_CAVE_TALK_RECEIVE_HEADER:
+        CavemanCaveTalk_LastReceive = CAVEMAN_CAVE_TALK_RECEIVE_PAYLOAD;
+        length                      = *(uint8_t*)((uint32_t)uart->rx_buffer.buffer +
+                                                  (uint32_t)((uint32_t)uart->rx_buffer.unlocked * uart->rx_buffer.half_buffer_size)
+                                                  + (uint32_t)uart->rx_buffer.write_count[uart->rx_buffer.unlocked] - 1U);
+        (void)BspUart_StartReceive(BSP_UART_USER_COMMS, (size_t)length);
+        break;
+    case CAVEMAN_CAVE_TALK_RECEIVE_PAYLOAD:
+        CavemanCaveTalk_LastReceive = CAVEMAN_CAVE_TALK_RECEIVE_HEADER;
+        (void)BspUart_StartReceive(BSP_UART_USER_COMMS, (size_t)length);
+        break;
+    default:
+        break;
+    }
+}
+
+static CaveTalk_Error_t CavemanCaveTalk_ConvertBspError(const Bsp_Error_t bsp_error)
+{
+    CaveTalk_Error_t cavetalk_error = CAVE_TALK_ERROR_NONE;
+
+    /* BSP UART functions should not return BSP_ERROR_VALUE */
     if (BSP_ERROR_NULL == bsp_error)
     {
         cavetalk_error = CAVE_TALK_ERROR_NULL;
@@ -56,27 +108,4 @@ static CaveTalk_Error_t CavemanCaveTalk_Send(const void *const data, const size_
     }
 
     return cavetalk_error;
-}
-
-static CaveTalk_Error_t CavemanCaveTalk_Receive(void *const data, const size_t size, size_t *const bytes_received)
-{
-    CAVE_TALK_UNUSED(data);
-    CAVE_TALK_UNUSED(size);
-    CAVE_TALK_UNUSED(bytes_received);
-
-    return CAVE_TALK_ERROR_NONE;
-}
-
-static CaveTalk_Error_t CavemanCaveTalk_Available(size_t *const bytes_available)
-{
-    CAVE_TALK_UNUSED(bytes_available);
-
-    return CAVE_TALK_ERROR_NONE;
-}
-
-static void CavemanCaveTalk_RxCallback(Bsp_Uart_t *const uart)
-{
-    BSP_UNUSED(uart);
-
-    /* TODO */
 }

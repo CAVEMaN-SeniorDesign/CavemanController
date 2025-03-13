3d72bb19d5f53a51449e6254f074682ff7000817
delta-12 <btesq235@gmail.com>
2025-03-12 22:44:28 -0400

    SD-233 initialize timers

--- bsp/boards/CAVeBoard/bsp_user/src/bsp_user.c
+++ bsp/boards/CAVeBoard/bsp_user/src/bsp_user.c
@@ -14,6 +14,15 @@ void BspUser_Initialize(void)
     MX_USART6_UART_Init();
     MX_TIM1_Init();
     MX_TIM2_Init();
+    MX_TIM3_Init();
+    MX_TIM4_Init();
+    MX_TIM5_Init();
+    MX_TIM6_Init();
+    MX_TIM8_Init();
     MX_TIM9_Init();
+    MX_TIM10_Init();
+    MX_TIM11_Init();
     MX_TIM12_Init();
+    MX_TIM13_Init();
+    MX_TIM14_Init();
 }

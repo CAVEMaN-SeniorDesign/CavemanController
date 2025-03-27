6eb15a5386d2a3e4ef78e016f42803c2a37f9f6c
delta-12 <btesq235@gmail.com>
2025-03-26 21:02:07 -0400

    SD-26 convert accelerometer fs2 to m/s^2

--- bsp/boards/CAVeBoard/rover_config/src/rover_imu_config.c
+++ bsp/boards/CAVeBoard/rover_config/src/rover_imu_config.c
@@ -14,9 +14,10 @@
 
 #include "rover.h"
 
-#define ROVER_IMU_BOOT_TIME     (Bsp_Millisecond_t)10U
-#define ROVER_IMU_TIMEOUT       (Bsp_Millisecond_t)100U
-#define ROVER_IMU_REGISTER_READ 0x80U
+#define ROVER_IMU_CONFIG_BOOT_TIME                        (Bsp_Millisecond_t)10U
+#define ROVER_IMU_CONFIG_TIMEOUT                          (Bsp_Millisecond_t)10U
+#define ROVER_IMU_CONFIG_REGISTER_READ                    0x80U
+#define ROVER_IMU_CONFIG_FS2_TO_METERS_PER_SECOND_SQUARED (double)5.985e-4
 
 typedef enum
 {
@@ -31,6 +32,7 @@ static const char * kRoverImuConfig_LogTag = "ROVER IMU CONFIG";
 static int32_t RoverImuConfig_Write(void *const handle, const uint8_t imu_register, const uint8_t *const data, const uint16_t size);
 static int32_t RoverImuConfig_Read(void *const handle, const uint8_t imu_register, uint8_t *const data, const uint16_t size);
 static void RoverImuConfig_ReadAll(void);
+static inline Rover_MetersPerSecondSquared_t RoverImuConfig_Fs2ToMetersPerSecondSquared(const int16_t fs2);
 
 static stmdev_ctx_t RoverImuConfig_DeviceHandle = {
     .write_reg = RoverImuConfig_Write,
@@ -47,7 +49,7 @@ Rover_Error_t RoverImuConfig_Initialize(void)
     Rover_Error_t error = ROVER_ERROR_NONE;
 
     /* Wait sensor boot time */
-    Bsp_Delay(ROVER_IMU_BOOT_TIME);
+    Bsp_Delay(ROVER_IMU_CONFIG_BOOT_TIME);
 
     /* Check device ID */
     uint8_t whoami;
@@ -85,7 +87,7 @@ Rover_Error_t RoverImuConfig_Initialize(void)
 
         /* Set full scale */
         lsm6dsv16x_xl_full_scale_set(&RoverImuConfig_DeviceHandle, LSM6DSV16X_2g);
-        lsm6dsv16x_gy_full_scale_set(&RoverImuConfig_DeviceHandle, LSM6DSV16X_2000dps);
+        lsm6dsv16x_gy_full_scale_set(&RoverImuConfig_DeviceHandle, LSM6DSV16X_125dps);
 
         /* Configure filtering chain */
         lsm6dsv16x_filt_settling_mask_t filter_settling_mask;
@@ -112,10 +114,9 @@ Rover_Error_t RoverImuConfig_ReadAccelerometer(Rover_AccelerometerReading_t *con
     {
         RoverImuConfig_ReadAll();
 
-        /* TODO convert to m/s^2 */
-        reading->x = (Rover_MetersPerSecondSquared_t)lsm6dsv16x_from_fs2_to_mg(RoverImuConfig_RawAccelerometer[ROVER_IMU_CONFIG_AXIS_X]);
-        reading->y = (Rover_MetersPerSecondSquared_t)lsm6dsv16x_from_fs2_to_mg(RoverImuConfig_RawAccelerometer[ROVER_IMU_CONFIG_AXIS_Y]);
-        reading->z = (Rover_MetersPerSecondSquared_t)lsm6dsv16x_from_fs2_to_mg(RoverImuConfig_RawAccelerometer[ROVER_IMU_CONFIG_AXIS_Z]);
+        reading->x = RoverImuConfig_Fs2ToMetersPerSecondSquared(RoverImuConfig_RawAccelerometer[ROVER_IMU_CONFIG_AXIS_X]);
+        reading->y = RoverImuConfig_Fs2ToMetersPerSecondSquared(RoverImuConfig_RawAccelerometer[ROVER_IMU_CONFIG_AXIS_Y]);
+        reading->z = RoverImuConfig_Fs2ToMetersPerSecondSquared(RoverImuConfig_RawAccelerometer[ROVER_IMU_CONFIG_AXIS_Z]);
 
         error = ROVER_ERROR_NONE;
     }
@@ -146,8 +147,8 @@ static int32_t RoverImuConfig_Write(void *const handle, const uint8_t imu_regist
 {
     (void)BspGpio_Write(BSP_GPIO_USER_PIN_IMU_CS, BSP_GPIO_STATE_RESET);
 
-    (void)HAL_SPI_Transmit(handle, &imu_register, 1U, ROVER_IMU_TIMEOUT);
-    (void)HAL_SPI_Transmit(handle, data, size, ROVER_IMU_TIMEOUT);
+    (void)HAL_SPI_Transmit(handle, &imu_register, 1U, ROVER_IMU_CONFIG_TIMEOUT);
+    (void)HAL_SPI_Transmit(handle, data, size, ROVER_IMU_CONFIG_TIMEOUT);
 
     (void)BspGpio_Write(BSP_GPIO_USER_PIN_IMU_CS, BSP_GPIO_STATE_SET);
 
@@ -156,12 +157,12 @@ static int32_t RoverImuConfig_Write(void *const handle, const uint8_t imu_regist
 
 static int32_t RoverImuConfig_Read(void *const handle, const uint8_t imu_register, uint8_t *const data, const uint16_t size)
 {
-    uint8_t register_read = imu_register | ROVER_IMU_REGISTER_READ;
+    uint8_t register_read = imu_register | ROVER_IMU_CONFIG_REGISTER_READ;
 
     (void)BspGpio_Write(BSP_GPIO_USER_PIN_IMU_CS, BSP_GPIO_STATE_RESET);
 
-    (void)HAL_SPI_Transmit(handle, &register_read, 1U, ROVER_IMU_TIMEOUT);
-    (void)HAL_SPI_Receive(handle, data, size, ROVER_IMU_TIMEOUT);
+    (void)HAL_SPI_Transmit(handle, &register_read, 1U, ROVER_IMU_CONFIG_TIMEOUT);
+    (void)HAL_SPI_Receive(handle, data, size, ROVER_IMU_CONFIG_TIMEOUT);
 
     (void)BspGpio_Write(BSP_GPIO_USER_PIN_IMU_CS, BSP_GPIO_STATE_SET);
 
@@ -182,4 +183,9 @@ static void RoverImuConfig_ReadAll(void)
     {
         lsm6dsv16x_angular_rate_raw_get(&RoverImuConfig_DeviceHandle, RoverImuConfig_RawGyroscope);
     }
+}
+
+static inline Rover_MetersPerSecondSquared_t RoverImuConfig_Fs2ToMetersPerSecondSquared(const int16_t fs2)
+{
+    return fs2 * ROVER_IMU_CONFIG_FS2_TO_METERS_PER_SECOND_SQUARED;
 }

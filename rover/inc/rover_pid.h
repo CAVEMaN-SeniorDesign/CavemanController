360891f078be024a9ea2f7cf86df3c49c14e2b72
delta-12 <btesq235@gmail.com>
2025-04-02 19:50:29 -0400

    SD-125 pid reset

--- rover/inc/rover_pid.h
+++ rover/inc/rover_pid.h
@@ -13,6 +13,7 @@ typedef struct
     double Output;
 } RoverPid_Handle_t;
 
+Rover_Error_t RoverPid_Reset(RoverPid_Handle_t *const handle);
 Rover_Error_t RoverPid_Update(RoverPid_Handle_t *const handle, const double command, const double actual, const Rover_Microsecond_t delta);
 
 #endif // ROVER_PID_H
--- rover/src/rover_pid.c
+++ rover/src/rover_pid.c
@@ -2,18 +2,32 @@
 
 #include "rover.h"
 
+Rover_Error_t RoverPid_Reset(RoverPid_Handle_t *const handle)
+{
+    Rover_Error_t error = ROVER_ERROR_NULL;
+
+    if (NULL != handle)
+    {
+        handle->Integral = 0.0;
+        handle->Error    = 0.0;
+        handle->Output   = 0.0;
+    }
+
+    return error;
+}
+
 Rover_Error_t RoverPid_Update(RoverPid_Handle_t *const handle, const double command, const double actual, const Rover_Microsecond_t delta)
 {
     Rover_Error_t error = ROVER_ERROR_NULL;
 
     if (NULL != handle)
     {
-        double pid_error = command - actual;
+        double pid_error  = command - actual;
         double derivative = (pid_error - handle->Error) / delta;
         handle->Integral += pid_error * delta;
 
         handle->Output = (handle->Kp * pid_error) + (handle->Ki * handle->Integral) + (handle->Kd * derivative);
-        handle->Error = pid_error;
+        handle->Error  = pid_error;
     }
 
     return error;
